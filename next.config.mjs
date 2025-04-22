9a6661ecb8d0e88d05de23bfc5ef0004c20a4e2a
DYNOTAWFEEQ <tawfeeq@studentdiwan.com>
2025-04-22 09:36:47 +0530

    image issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images: {unoptimized: true},
-};
-
-export default nextConfig;
+    images: {
+      unoptimized: true, 
+      domains: ['https://www.rameentourandtravels.com'],  
+    },
+  };
+  
+  export default nextConfig;
+  
